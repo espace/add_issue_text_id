cf5a947c8ab95b6237eb77319ded3085a96815cf
Hager Abdelmotaal <hager.abdelmotaal@espace.com.eg>
2012-09-17 15:51:24 +0200

    change patching mechanism for rails 3  - trial 3

--- lib/issue_patcher.rb
+++ lib/issue_patcher.rb
@@ -2,17 +2,32 @@ require_dependency 'issue'
 require 'application_helper'
 
 module IssuePatcher
-  def self.included(base)
-    base.class_eval do
-      unloadable
-      validates_uniqueness_of :text_id, :scope=>'project_id' ,:allow_blank=>true
+  module Patches
+    module ApplicationHelperPatch
+      def self.included(base) # :nodoc:
+        # sending instance methods to module
+        base.send(:include, InstanceMethods)
+
+        base.class_eval do
+          unloadable
+
+          # aliasing methods if needed
+          alias_method_chain :validates_text_id, :checkers
+        end
+      end
+
+      # Instance methods are here
+      module InstanceMethods
+        def validates_text_id_with_checkers
+            validates_uniqueness_of :text_id, :scope=>'project_id' ,:allow_blank=>true
+        end
+      end
     end
   end
 end
 
-
 # now we should include this module in ApplicationHelper module
-unless ApplicationHelper.included_modules.include? IssuePatcher
-    ApplicationHelper.send(:include, IssuePatcher )
+unless ApplicationHelper.included_modules.include? IssuePatcher::Patches::ApplicationHelperPatch
+    ApplicationHelper.send(:include, IssuePatcher::Patches::ApplicationHelperPatch)
 end
 
