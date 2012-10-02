48a6456e7099c44b5373a08eb83ee22dcda3ab92
Yasmine Gaber <yasmine.gaber@espace.com.eg>
2012-10-02 13:34:51 +0200

    upgrade to redmine 2.1

--- lib/issue_patcher.rb
+++ lib/issue_patcher.rb
@@ -1,10 +1,20 @@
 require_dependency 'issue'
 module IssuePatcher
   def self.included(base)
+    Issue.safe_attributes 'text_id', :if => lambda {|issue, user| issue.new_statuses_allowed_to(user).any? }
+
     base.class_eval do
       unloadable
       validates_uniqueness_of :text_id, :scope=>'project_id' ,:allow_blank=>true
     end
+    base.send(:extend,ClassMethods)
   end
+  module ClassMethods
+    def has_story_tracker?(project)
+      found=project.trackers.find_by_name("Story")
+      return found.id  if found
+    end
+  end
+
 end
 
